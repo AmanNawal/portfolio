4a679d3d8193a5a0393016c411e3709204ff8d55
Aman Nawal <amannawal@gmail.com>
2023-07-10 20:35:22 +0530

    animations added 2

--- portfolio_1/logic.js
+++ portfolio_1/logic.js
@@ -1,26 +1,41 @@
-const observer = new IntersectionObserver((entries) => {
+const observerSide = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show-left-right');
             entry.target.classList.add('show-right-left');
-            entry.target.classList.add('skill-level');
+            
         }
         else
         {
             
             entry.target.classList.remove('show-left-right');
             entry.target.classList.remove('show-right-left');
-            entry.target.classList.remove('skill-level');
+            
         }
     });
     });
 
 const hiddenLeftToRight = document.querySelectorAll('.hidden-left-right');
 const hiddenRightToLeft = document.querySelectorAll('.hidden-right-left');
-const skillLevel = document.querySelectorAll('.skill-level');
-hiddenLeftToRight.forEach((element) => {observer.observe(element)});
-hiddenRightToLeft.forEach((element) => {observer.observe(element)});
-skillLevel.forEach((element) => {observer.observe(element)});
+
+hiddenLeftToRight.forEach((element) => {observerSide.observe(element)});
+hiddenRightToLeft.forEach((element) => {observerSide.observe(element)});
+
+
+const observerProgressBar = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('skill-level');
+        }
+        else
+        {
+            entry.target.classList.remove('skill-level');
+        }
+    });
+    });
+
+    const skillLevel = document.querySelectorAll('.skill-level');
+    skillLevel.forEach((element) => {observerProgressBar.observe(element)});
 
 /*Intersection Observer: is a browser API that allows you to determine when an element in the DOM enters the browser’s viewport.*/
 /*entries: is an array of objects that contain information about each observed element.*/
